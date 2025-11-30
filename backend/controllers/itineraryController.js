308d92ac0720057d7944158cadaf93c408c7c350
Akhileswaran-K-R <akhilarathi956@gmail.com>
2025-11-30 09:10:18 +0530

    minor change for sorting schedule

--- backend/controllers/itineraryController.js
+++ backend/controllers/itineraryController.js
@@ -3,16 +3,13 @@ const { db } = require("../config/firebase");
 async function getItineraries() {
   try {
     const itinerariesArray = [];
-    const snapshot = await db.collection("itineraries").get();
+    const snapshot = await db
+      .collection("itineraries")
+      .orderBy("dayNumber")
+      .get();
 
     snapshot.forEach((doc) => {
       itinerariesArray.push(doc.data());
-    });
-
-    itinerariesArray.sort((a, b) => {
-      const dayA = parseInt(a.day?.match(/\d+/)?.[0] || 0);
-      const dayB = parseInt(b.day?.match(/\d+/)?.[0] || 0);
-      return dayA - dayB;
     });
 
     return itinerariesArray;
