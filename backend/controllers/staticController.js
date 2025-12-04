d4b669576937d44c5c90ad419a3ab5757cde5b09
Akhileswaran-K-R <akhilarathi956@gmail.com>
2025-12-04 23:56:39 +0530

    connected with service account to fetch gsheet

--- backend/controllers/staticController.js
+++ backend/controllers/staticController.js
@@ -1,4 +1,7 @@
+const { google } = require("googleapis");
 const { db } = require("../config/firebase");
+const credentials = require("../credentials.json");
+require("dotenv").config();
 
 async function getItineraries() {
   try {
@@ -35,7 +38,28 @@ async function getUtensils() {
   }
 }
 
+async function getSheetData() {
+  const auth = new google.auth.GoogleAuth({
+    credentials: credentials,
+    scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
+  });
+
+  const client = await auth.getClient();
+  const sheets = google.sheets({ version: "v4", auth: client });
+
+  const { spreadsheetId } = process.env;
+  const range = "Sheet1!A1:B2";
+
+  const response = await sheets.spreadsheets.values.get({
+    spreadsheetId,
+    range,
+  });
+
+  return response.data.values;
+}
+
 module.exports = {
   getItineraries,
   getUtensils,
+  getSheetData,
 };
--- backend/utils/tokens.js
+++ backend/utils/tokens.js
@@ -22,4 +22,30 @@ function generateTokens(user) {
   return { accessToken, refreshToken };
 }
 
-module.exports = { generateTokens };
+function verifyAccessToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  if (!authHeader) {
+    return res.status(401).json({ message: "Authorization header missing" });
+  }
+
+  const [scheme, token] = authHeader.split(" ");
+  if (scheme !== "Bearer") {
+    return res
+      .status(401)
+      .json({ message: "Invalid auth scheme. Use Bearer token." });
+  }
+
+  if (!token) {
+    return res.status(401).json({ message: "Access token missing" });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: "Invalid or expired token" });
+    }
+    req.user = decoded;
+    next();
+  });
+}
+
+module.exports = { generateTokens, verifyAccessToken };
