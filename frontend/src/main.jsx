3fa5d704d88c8f122c82362ec6345319d3cfabf5
Abhai Sankar P R <165683267+AbhaiSankarPR@users.noreply.github.com>
2025-12-10 13:18:17 +0530

    tailwind update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
-import { AuthProvider } from "./pages/AuthPage/AuthContext"; // ✅ import the provider
+import { AuthProvider } from "./pages/AuthPage/AuthContext"; 
 import "./index.css";
 import "leaflet/dist/leaflet.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>   {/* ✅ wrap the entire app here */}
+    <AuthProvider> 
       <RouterProvider router={router} />
     </AuthProvider>
   </React.StrictMode>
