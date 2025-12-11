1888e94e23e60627acd619517b105358e23a9969
Akhileswaran-K-R <akhilarathi956@gmail.com>
2025-12-11 20:52:48 +0530

    removed sheets

--- backend/controllers/staticController.js
+++ backend/controllers/staticController.js
@@ -1,6 +1,4 @@
-const { google } = require("googleapis");
 const { db } = require("../config/firebase");
-const credentials = require("../credentials.json");
 
 async function getItineraries() {
   try {
@@ -37,28 +35,7 @@ async function getUtensils() {
   }
 }
 
-async function getSheetData() {
-  const auth = new google.auth.GoogleAuth({
-    credentials: credentials,
-    scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
-  });
-
-  const client = await auth.getClient();
-  const sheets = google.sheets({ version: "v4", auth: client });
-
-  const { spreadsheetId } = process.env;
-  const range = "Sheet1!A1:B2";
-
-  const response = await sheets.spreadsheets.values.get({
-    spreadsheetId,
-    range,
-  });
-
-  return response.data.values;
-}
-
 module.exports = {
   getItineraries,
   getUtensils,
-  getSheetData,
 };
--- backend/routes/staticRoutes.js
+++ backend/routes/staticRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const {
   getItineraries,
   getUtensils,
-  getSheetData,
 } = require("../controllers/staticController");
 const { verifyAccessToken } = require("../utils/tokens");
 
@@ -15,16 +14,6 @@ router.get("/itinerary", async (req, res) => {
 router.get("/utensils", async (req, res) => {
   const data = await getUtensils();
   res.json(data);
-});
-
-router.get("/amount", verifyAccessToken, async (req, res) => {
-  try {
-    const data = await getSheetData();
-    res.json({ data });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Unable to fetch Google Sheet data" });
-  }
 });
 
 module.exports = router;
