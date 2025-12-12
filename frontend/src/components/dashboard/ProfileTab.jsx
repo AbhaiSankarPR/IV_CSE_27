76a782fc8bd02c0470ae343b0c99c657454a93fc
Akhileswaran-K-R <akhilarathi956@gmail.com>
2025-12-12 21:34:08 +0530

    updated qr code

--- frontend/src/components/dashboard/ProfileTab.jsx
+++ frontend/src/components/dashboard/ProfileTab.jsx
@@ -4,7 +4,8 @@ import { useState } from "react";
 
 export default function ProfileTab({ user }) {
   const [showQRCode, setShowQRCode] = useState(false);
-  const upiLink = "upi://pay?pa=abhaisankarpr@oksbi&pn=Abhai%20Sankar%20P%20R&aid=uGICAgMDuns7SVQu";
+  const upiLink =
+    "upi://pay?pa=abhaisankarpr@oksbi&pn=Abhai%20Sankar%20P%20R&cu=INR";
 
   const handlePay = () => {
     if (/android|iphone|ipad|ipod/i.test(navigator.userAgent)) {
@@ -18,7 +19,8 @@ export default function ProfileTab({ user }) {
     <div className="pl-0 pr-0 md:pl-80 p-4 min-h-screen bg-[#0d0d0d]/30 flex flex-col items-center">
       <div className="w-full max-w-3xl bg-white/10 backdrop-blur-xl p-6 rounded-xl shadow-lg text-center md:text-left">
         <h1 className="text-3xl md:text-4xl font-bold mb-3">
-          Welcome, <span className="text-green-400">{user?.name || "User"}</span>
+          Welcome,{" "}
+          <span className="text-green-400">{user?.name || "User"}</span>
         </h1>
         <p className="text-gray-300 mb-2 text-base md:text-lg">
           You are successfully logged in.
@@ -38,13 +40,22 @@ export default function ProfileTab({ user }) {
             className="w-9 h-9 object-contain invert brightness-200"
             alt="Tea"
           />
-          <span className="text-lg sm:text-xl font-semibold cursor-pointer">Buy me a tea</span>
+          <span className="text-lg sm:text-xl font-semibold cursor-pointer">
+            Buy me a tea
+          </span>
         </button>
 
         {showQRCode && (
           <div className="mt-4 flex flex-col items-center">
-            <QRCode value={upiLink} size={180} bgColor="transparent" fgColor="white" />
-            <p className="text-sm text-gray-300 mt-2 text-center">Scan to pay via UPI</p>
+            <QRCode
+              value={upiLink}
+              size={180}
+              bgColor="transparent"
+              fgColor="white"
+            />
+            <p className="text-sm text-gray-300 mt-2 text-center">
+              Scan to pay via UPI
+            </p>
           </div>
         )}
       </div>
